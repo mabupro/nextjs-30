5350727cb54035317a57ff04b2e462d499bd7e43
mabupro <mabupro787@gmail.com>
2024-02-01 14:35:34 +0900

    create/calendarpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 // import Counter from "@/app/src/pages/counter/page";
 // import HelloWorld from "@/app/src/pages/helloworld/page";
 // import Timer from "@/app/src/pages/timer/page";
-import ShowDay from "@/app/src/pages/showday/page";
+// import ShowDay from "@/app/src/pages/showday/page";
+import Calendar from "@/app/src/pages/calendar/page";
 
 export default function Home() {
   return (
     // <HelloWorld />
     // <Counter />
     // <Timer />
-    <ShowDay />
+    // <ShowDay />
+    <Calendar />
   );
 }
---
+++ app/src/pages/calendar/page.tsx
@@ -0,0 +1,11 @@
+const Calendar = () => {
+    return(
+        <>
+            <div className="m-10 bg-slate-200">
+                day
+            </div>
+        </>
+    )
+}
+
+export default Calendar
