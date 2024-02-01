0f305f63017a6b1eeeeead1ddc308e146f7ed8ad
mabupro <mabupro787@gmail.com>
2024-02-01 16:05:17 +0900

    create/calendarpage

--- app/src/pages/calendar/page.tsx
+++ app/src/pages/calendar/page.tsx
@@ -1,8 +1,16 @@
 const Calendar = () => {
-    return(
+    return (
         <>
-            <div className="m-10 bg-slate-200">
-                day
+            <div className="mx-auto text-center mt-32 w-1/2 h-1/2 rounded-md text-xl font-semibold bg-amber-100">
+                カレンダー
+            </div>
+            <div className="mx-auto w-1/2 rounded-md text-lg bg-slate-100">
+                <div className="text-center ">
+                    2024年1月
+                </div>
+                <div className="">
+                    1
+                </div>
             </div>
         </>
     )
